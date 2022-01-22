6b89ec39cb64c44fe440a87f45975c8a886fff47
Shahzaneer Ahmed <shahism01@gmail.com>
2022-01-23 01:28:06 +0500

    dynamic keyword file initial commit

---
+++ dynamic.dart
@@ -0,0 +1,11 @@
+// we can use dynamic keyword to declare 
+//a variable dynamically like python
+
+main(){
+  dynamic alpha = 'Shahzaneer Ahmed - Data Scientist';
+  print(alpha);
+  // now I can change the alpha to numeric type or any other type as well
+alpha = 45; 
+print(alpha); //this is dynamic things!
+
+}
