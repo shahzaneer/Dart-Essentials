3267bbb0133fe2e0549327aa447184ee54101e07
Shahzaneer Ahmed <shahism01@gmail.com>
2022-01-23 01:34:40 +0500

    arrowfunction initial commit

---
+++ arrowfunctions.dart
@@ -0,0 +1,6 @@
+// arrow functions are one line functions 
+// these are similar as javascript
+
+main() => print("shahznaeer ahmed -- the AI Enthusiast");
+// just remember the "equal and greater than symbol to form arrow" =>
+ 
