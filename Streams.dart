a2c491dba16df65848f572d8481eef5cf080bbe0
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-08-08 02:35:04 +0500

    Streams (pipelines for asynchronous data)

---
+++ Streams.dart
@@ -0,0 +1,21 @@
+Stream<String> getName() {
+  // return Stream.value("Shahzaneer Ahmed");
+  // yeh tou aik value return ki hai jbke stream continuosly krskti hai return isliay 
+  return Stream.periodic(const Duration(seconds: 1), ((int d) => "ShahzaneerAhmed"));
+  // ab har second baad shahzaneerAhmed print hota rahega yeh asynchronous programming hai!
+}
+
+// Stream is the pipeline of asynchronous data
+
+void runner() async {
+  await for (final value in getName()) {
+    print(value);
+  }
+  print("The data from the streamline just printed to the console!");
+}
+
+
+
+main(){
+  runner();
+}
